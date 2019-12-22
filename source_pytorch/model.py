2bb9b98626100e3f011c39fa0d6ef5d6387b0b60
Todd Cunningham <tcunningham07@gmail.com>
2019-12-22 20:07:51 +1000

    Initial PyTorch training function + model

--- source_pytorch/model.py
+++ source_pytorch/model.py
@@ -3,7 +3,6 @@ import torch.nn.functional as F
 import torch.nn as nn
 
 
-## TODO: Complete this classifier
 class BinaryClassifier(nn.Module):
     """
     Define a neural network that performs binary classification.
@@ -15,7 +14,7 @@ class BinaryClassifier(nn.Module):
     BCELoss is binary cross entropy loss, documentation: https://pytorch.org/docs/stable/nn.html#torch.nn.BCELoss
     """
 
-    ## TODO: Define the init function, the input params are required (for loading code in train.py to work)
+    ## Define the init function, the input params are required (for loading code in train.py to work)
     def __init__(self, input_features, hidden_dim, output_dim):
         """
         Initialize the model by setting up linear layers.
@@ -25,12 +24,16 @@ class BinaryClassifier(nn.Module):
         :param output_dim: the number of outputs you want to produce
         """
         super(BinaryClassifier, self).__init__()
-
-        # define any initial layers, here
         
+        # defining 2 linear layers
+        self.fc1 = nn.Linear(input_features, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.drop = nn.Dropout(0.3)
+        # sigmoid layer
+        self.sig = nn.Sigmoid()
 
     
-    ## TODO: Define the feedforward behavior of the network
+    ## Define the feedforward behavior of the network
     def forward(self, x):
         """
         Perform a forward pass of our model on input features, x.
@@ -38,7 +41,8 @@ class BinaryClassifier(nn.Module):
         :return: A single, sigmoid-activated value as output
         """
         
-        # define the feedforward behavior
-        
-        return x
+        out = F.relu(self.fc1(x)) # activation on hidden layer
+        out = self.drop(out)
+        out = self.fc2(out)
+        return self.sig(out) # returning class score
     
--- source_pytorch/train.py
+++ source_pytorch/train.py
@@ -5,6 +5,8 @@ import pandas as pd
 import torch
 import torch.optim as optim
 import torch.utils.data
+import torch.nn as nn
+import numpy as np
 
 # imports the model in model.py by name
 from model import BinaryClassifier
@@ -42,7 +44,7 @@ def _get_train_data_loader(batch_size, training_dir):
 
     train_data = pd.read_csv(os.path.join(training_dir, "train.csv"), header=None, names=None)
 
-    train_y = torch.from_numpy(train_data[[0]].values).float().squeeze()
+    train_y = torch.from_numpy(np.array(train_data[[0]].values, dtype=np.uint8)).float().squeeze()
     train_x = torch.from_numpy(train_data.drop([0], axis=1).values).float()
 
     train_ds = torch.utils.data.TensorDataset(train_x, train_y)
@@ -91,7 +93,6 @@ def train(model, train_loader, epochs, criterion, optimizer, device):
         print("Epoch: {}, Loss: {}".format(epoch, total_loss / len(train_loader)))
 
 
-## TODO: Complete the main code
 if __name__ == '__main__':
     
     # All of the model parameters and training parameters are sent as arguments
@@ -114,8 +115,13 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
     
-    ## TODO: Add args for the three model parameters: input_features, hidden_dim, output_dim
-    # Model Parameters
+    # Model parameters
+    parser.add_argument('--input_dim', type=int, default=2, metavar='IN',
+                        help='number of input features to model (default: 2)')
+    parser.add_argument('--hidden_dim', type=int, default=10, metavar='H',
+                        help='hidden dim of model (default: 10)')
+    parser.add_argument('--output_dim', type=int, default=1, metavar='OUT',
+                        help='output dim of model (default: 1)')
     
     
     # args holds all passed-in arguments
@@ -129,29 +135,24 @@ if __name__ == '__main__':
     # Load the training data.
     train_loader = _get_train_data_loader(args.batch_size, args.data_dir)
 
-
-    ## --- Your code here --- ##
-    
-    ## TODO:  Build the model by passing in the input params
     # To get params from the parser, call args.argument_name, ex. args.epochs or ards.hidden_dim
     # Don't forget to move your model .to(device) to move to GPU , if appropriate
-    model = None
+    model = BinaryClassifier(args.input_dim, args.hidden_dim, args.output_dim).to(device)
 
-    ## TODO: Define an optimizer and loss function for training
-    optimizer = None
-    criterion = None
+    # Define optimizer and loss function for training
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    criterion = nn.BCELoss()
 
     # Trains the model (given line of code, which calls the above training function)
     train(model, train_loader, args.epochs, criterion, optimizer, device)
 
-    ## TODO: complete in the model_info by adding three argument names, the first is given
     # Keep the keys of this dictionary as they are 
     model_info_path = os.path.join(args.model_dir, 'model_info.pth')
     with open(model_info_path, 'wb') as f:
         model_info = {
-            'input_features': args.input_features,
-            'hidden_dim': <add_arg>,
-            'output_dim': <add_arg>,
+            'input_features': args.input_dim,
+            'hidden_dim': args.hidden_dim,
+            'output_dim': args.output_dim,
         }
         torch.save(model_info, f)
         
